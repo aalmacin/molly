195f99f1af7bb1b9c11ba5dce5a4ab2c7f29c2e1
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-03 14:31:56 -0400

    Working component test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
+  preset: 'jest-preset-angular',
   globals: {
     __TRANSFORM_HTML__: true
   },
   testPathIgnorePatterns: ["/node_modules/", "/projects/"],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.(ts|html)$': 'jest-preset-angular'
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', '.html'],
 };
