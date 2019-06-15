2f11c0afd84d22e9d61833f8dd43d777df177b86
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-06-15 00:07:10 -0400

    Added text transform

---
+++ src/stories/css-library/typography/text-transform.stories.ts
@@ -0,0 +1,24 @@
+import { storiesOf } from "@storybook/angular";
+import { text } from '@storybook/addon-knobs';
+
+const stories = storiesOf("CSS Library/Typography/Text Transform", module);
+
+stories.add("All", () => ({
+  props: {
+    items: [
+      { desc: "none", class: "" },
+      { desc: "uppercase", class: "mol-tt-u" },
+      { desc: "lowercase", class: "mol-tt-l" },
+      { desc: "capitalize", class: "mol-tt-c" },
+    ],
+    text: text('Text', 'The quick brown fox jumps over the lazy dog')
+  },
+  template: `
+    <ng-container *ngFor="let item of items">
+      <h3>{{ item.desc }} <span *ngIf="item.class">({{ item.class }})</span></h3>
+      <div [class]="item.class">
+        {{ text }}
+      </div>
+    </ng-container>
+  `
+}));
--- src/stories/css-library/typography/type-scale.stories.ts
+++ src/stories/css-library/typography/type-scale.stories.ts
@@ -1,13 +1,33 @@
 import { storiesOf } from "@storybook/angular";
+import { text } from '@storybook/addon-knobs';
 
 const stories = storiesOf("CSS Library/Typography/TypeScale", module);
 
 stories.add("Type Scale", () => ({
-  props: {},
+  props: {
+    items: [
+      { desc: "small", class: "mol-f-sm" },
+      { desc: "base", class: "" },
+      { desc: "large", class: "mol-f-lg" },
+      { desc: "very large", class: "mol-f-vl" }
+    ],
+    text: text('Text', 'the quick brown fox jumps over the lazy dog')
+  },
   template: `
-    <span class="mol-f-sm">[ mol-f-sm ]</span>
-    <span>[ base ]</span>
-    <span class="mol-f-lg">[ mol-f-lg ]</span>
-    <span class="mol-f-vl">[ mol-f-vl ]</span>
+    <ng-container *ngFor="let item of items">
+      <span [class]="item.class">
+        [ {{ item.class || 'base' }} ]
+      </span>
+    </ng-container>
+
+    <hr>
+
+    <ng-container *ngFor="let item of items">
+      <h3>{{ item.desc }} <span *ngIf="item.class">({{ item.class }})</span></h3>
+      <div [class]="item.class">
+        {{ text }}
+        <span class="mol-t-uc">{{ text }}</span>
+      </div>
+    </ng-container>
   `
 }));
