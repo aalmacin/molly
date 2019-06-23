d9dd28cd56318e5a1c4b621a1c5f79e5303193c1
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-06-23 00:41:04 -0400

    Updated font size of card

--- projects/molly-component-library/src/lib/card/card.component.ts
+++ projects/molly-component-library/src/lib/card/card.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'mol-card',
   styleUrls: ['./card.component.scss'],
   template: `
-    <div class="mol-card">
+    <div class="mol-card mol-tf-lg">
       <div
         class="mol-sb-secondary-light-x mol-sb-secondary-light-t mol-sbg-secondary-light mol-sc-white mol-tta-c mol-lp-sm mol-tff-h"
       >
